f6a2032dadf3f409d98da054c35d3a39bc4ab05e
bl33h <per21371@uvg.edu.gt>
2023-06-04 18:08:19 -0600

    general elements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: App.jsx
+   Version: I
+   Creation: 04/06/2023
+   Last modification: 04/06/2023
+*/
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,12 +16,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,12 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: main.jsx
+   Version: I
+   Creation: 04/06/2023
+   Last modification: 04/06/2023
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
